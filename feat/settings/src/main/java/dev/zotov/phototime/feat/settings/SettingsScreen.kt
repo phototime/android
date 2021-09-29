be5da5d3a43558b7e43d2a83ceca3f05efe461bb
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-09-29 13:16:31 +0500

    use FullScreenMessage component on not found settings screen

--- feat/settings/src/main/java/dev/zotov/phototime/feat/settings/SettingsScreen.kt
+++ feat/settings/src/main/java/dev/zotov/phototime/feat/settings/SettingsScreen.kt
@@ -1,19 +1,14 @@
 package dev.zotov.phototime.feat.settings
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import dev.zotov.phototime.shared.components.FullScreenMessage
 import dev.zotov.phototime.shared.components.Headline
-import dev.zotov.phototime.shared.theme.subtitle
 
 @Composable
 fun SettingsScreen(navController: NavHostController, scrollState: ScrollState) {
@@ -25,30 +20,14 @@ fun SettingsScreen(navController: NavHostController, scrollState: ScrollState) {
 
 @Composable
 private fun ColumnScope.Message() {
-    Spacer(modifier = Modifier.weight(4f))
-    CoolIcon()
-    Spacer(modifier = Modifier.height(25.dp))
-    MessageText()
-    Spacer(modifier = Modifier.weight(5f))
-}
-
-@Composable
-private fun CoolIcon() {
-    val painter = painterResource(id = R.drawable.cool)
-    Image(
-        painter = painter,
-        contentDescription = "cool icon",
-        modifier = Modifier.size(56.dp)
-    )
-}
-
-@Composable
-private fun MessageText() {
-    Text(
+    FullScreenMessage(
+        iconId = R.drawable.cool,
         text = "The developers are very lazy and have not yet made this screen\nA lot of cool stuff coming soon!",
-        modifier = Modifier.width(202.dp),
-        style = MaterialTheme.typography.subtitle,
+        startWeight = 4f,
+        endWeight = 5f,
+        contentDescription = "cool icon",
     )
+
 }
 
 @Composable
