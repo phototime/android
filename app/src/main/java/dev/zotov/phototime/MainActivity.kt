73154591ec31d286265f5ddb46e77e3c1b4de0c1
Yaroslav Zotov <35657233+zotovY@users.noreply.github.com>
2021-11-01 22:25:28 +0500

    refactor(MainActivity): gps

--- app/src/main/java/dev/zotov/phototime/MainActivity.kt
+++ app/src/main/java/dev/zotov/phototime/MainActivity.kt
@@ -2,28 +2,20 @@ package dev.zotov.phototime
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.google.android.gms.tasks.Task
 import com.vmadalin.easypermissions.EasyPermissions
 import dev.zotov.phototime.shared.Routes
 import dev.zotov.phototime.shared.theme.PhototimeTheme
-import dev.zotov.phototime.shared.usecases.FetchForecastUseCase
-import org.koin.android.ext.android.inject
 import com.google.android.gms.common.api.ApiException
 
-import android.content.IntentSender
 import android.content.IntentSender.SendIntentException
 import android.os.Looper
 
@@ -32,8 +24,6 @@ import com.google.android.gms.location.*
 
 
 class MainActivity : ComponentActivity(), EasyPermissions.PermissionCallbacks {
-
-    private val fetchForecastUseCase: FetchForecastUseCase by inject()
 
     companion object {
         const val PERMISSION_LOCATION_REQUEST_CODE = 10002
@@ -50,71 +40,8 @@ class MainActivity : ComponentActivity(), EasyPermissions.PermissionCallbacks {
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
         )
 
-        if (hasLocationPermission()) {
-            val locationRequest = LocationRequest.create()
-            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            locationRequest.interval = 60000
-            locationRequest.fastestInterval = 5000
-
-            val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
-            builder.setAlwaysShow(true)
-
-            val result: Task<LocationSettingsResponse> =
-                LocationServices.getSettingsClient(this@MainActivity)
-                    .checkLocationSettings(builder.build())
-
-            result.addOnCompleteListener { task ->
-                try {
-                    task.getResult(ApiException::class.java)
-                    val fusedLocationProviderClient =
-                        LocationServices.getFusedLocationProviderClient(this@MainActivity)
-
-                    fusedLocationProviderClient.requestLocationUpdates(
-                        locationRequest,
-                        object : LocationCallback() {
-                            override fun onLocationResult(p0: LocationResult) {
-                                super.onLocationResult(p0)
-                                Log.d("location", p0.toString())
-                                fusedLocationProviderClient.removeLocationUpdates(this)
-                            }
-                        },
-                        Looper.getMainLooper(),
-                    )
-                } catch (e: ApiException) {
-                    Log.e("location", e.toString())
-                    when (e.statusCode) {
-                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
-                            val resolvableApiException = e as ResolvableApiException
-                            resolvableApiException.startResolutionForResult(
-                                this@MainActivity,
-                                REQUEST_CHECK_SETTINGS
-                            )
-                            val fusedLocationProviderClient =
-                                LocationServices.getFusedLocationProviderClient(this@MainActivity)
-
-                            fusedLocationProviderClient.requestLocationUpdates(
-                                locationRequest,
-                                object : LocationCallback() {
-                                    override fun onLocationResult(p0: LocationResult) {
-                                        super.onLocationResult(p0)
-                                        Log.d("location", p0.toString())
-                                        fusedLocationProviderClient.removeLocationUpdates(this)
-                                    }
-                                },
-                                Looper.getMainLooper(),
-                            )
-                        } catch (ex: SendIntentException) {
-                            ex.printStackTrace()
-                        }
-                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                        }
-                    }
-                }
-            }
-        } else {
-            requestLocationPermission()
-        }
-
+        if (hasLocationPermission()) getAllPermissions()
+        else requestLocationPermission()
 
         setContent {
             PhototimeTheme {
@@ -155,81 +82,68 @@ class MainActivity : ComponentActivity(), EasyPermissions.PermissionCallbacks {
         TODO("Not yet implemented")
     }
 
-    @SuppressLint("MissingPermission")
     override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {
-        Log.d("onPermissionsGranted", requestCode.toString())
-
         if (requestCode == PERMISSION_LOCATION_REQUEST_CODE) {
-            val locationRequest = LocationRequest.create()
-            locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-            locationRequest.interval = 60000
-            locationRequest.fastestInterval = 5000
-
-            val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
-            builder.setAlwaysShow(true)
-
-            val result: Task<LocationSettingsResponse> =
-                LocationServices.getSettingsClient(this@MainActivity)
-                    .checkLocationSettings(builder.build())
-
-            result.addOnCompleteListener { task ->
-                try {
-                    task.getResult(ApiException::class.java)
-                    val fusedLocationProviderClient =
-                        LocationServices.getFusedLocationProviderClient(this@MainActivity)
-
-                    fusedLocationProviderClient.requestLocationUpdates(
-                        locationRequest,
-                        object : LocationCallback() {
-                            override fun onLocationResult(p0: LocationResult) {
-                                super.onLocationResult(p0)
-                                Log.d("location2", p0.toString())
-                                fusedLocationProviderClient.removeLocationUpdates(this)
-                            }
-                        },
-                        Looper.getMainLooper(),
-                    )
-                } catch (e: ApiException) {
-                    Log.e("location", e.toString())
-                    when (e.statusCode) {
-                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
-                            val resolvableApiException = e as ResolvableApiException
-                            resolvableApiException.startResolutionForResult(
-                                this@MainActivity,
-                                REQUEST_CHECK_SETTINGS
-                            )
-                            val fusedLocationProviderClient =
-                                LocationServices.getFusedLocationProviderClient(this@MainActivity)
-
-                            fusedLocationProviderClient.requestLocationUpdates(
-                                locationRequest,
-                                object : LocationCallback() {
-                                    override fun onLocationResult(p0: LocationResult) {
-                                        super.onLocationResult(p0)
-                                        Log.d("location", p0.toString())
-                                        fusedLocationProviderClient.removeLocationUpdates(this)
-                                    }
-                                },
-                                Looper.getMainLooper(),
-                            )
-                        } catch (ex: SendIntentException) {
-                            ex.printStackTrace()
-                        }
-                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
-                        }
-                    }
-                }
-            }
-
-
+            getAllPermissions()
         }
     }
 
-    private fun checkGPSPermissions(context: Context): Boolean {
-        val permissionState = ActivityCompat.checkSelfPermission(
-            context,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        )
-        return permissionState == PackageManager.PERMISSION_GRANTED
+    private fun getAllPermissions() {
+        val locationRequest = LocationRequest.create().apply {
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 60000
+            fastestInterval = 5000
+        }
+
+        val builder = LocationSettingsRequest.Builder().apply {
+            addLocationRequest(locationRequest)
+            setAlwaysShow(true)
+        }
+
+        val result = LocationServices.getSettingsClient(this@MainActivity)
+            .checkLocationSettings(builder.build())
+
+        result.addOnCompleteListener { task ->
+            try {
+                task.getResult(ApiException::class.java)
+                getAndSaveLocation(locationRequest)
+            } catch (e: ApiException) {
+                Log.e("location", e.toString())
+                when (e.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                        val resolvableApiException = e as ResolvableApiException
+                        resolvableApiException.startResolutionForResult(
+                            this@MainActivity,
+                            REQUEST_CHECK_SETTINGS
+                        )
+                        getAndSaveLocation(locationRequest)
+                    } catch (ex: SendIntentException) {
+                        ex.printStackTrace()
+                    }
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                    }
+                }
+            }
+        }
     }
+
+
+    @SuppressLint("MissingPermission")
+    private fun getAndSaveLocation(locationRequest: LocationRequest) {
+        val fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this@MainActivity)
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            object : LocationCallback() {
+                override fun onLocationResult(p0: LocationResult) {
+                    super.onLocationResult(p0)
+                    Log.d("location2", p0.toString())
+                    fusedLocationProviderClient.removeLocationUpdates(this)
+                }
+            },
+            Looper.getMainLooper(),
+        )
+    }
+
 }
