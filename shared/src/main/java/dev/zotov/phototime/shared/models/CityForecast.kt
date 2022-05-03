e8130514f87f83d668e8a8371bae79ff9f479ff9
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-05-03 14:38:50 +0500

    feat(domain): add `countryCode` to `CityForecast`

--- shared/src/main/java/dev/zotov/phototime/shared/models/CityForecast.kt
+++ shared/src/main/java/dev/zotov/phototime/shared/models/CityForecast.kt
@@ -1,5 +1,6 @@
 package dev.zotov.phototime.shared.models
 
+import androidx.annotation.Size
 import dev.zotov.phototime.domain.ForecastType
 
 /**
@@ -12,13 +13,22 @@ data class CityForecast(
     val city: String,
 
     /**
+     * Short two-character alphabetic geographical codes used to represent countries and dependent territories
+     *
+     * Example: RU for Russia, AU for Australia, GB for Great Britain
+     */
+    @Size(2)
+    val countryCode: String,
+
+    /**
      * Type of current weather (sunny, cloudy, rainy etc)
      */
     val type: ForecastType,
 
     /**
      * Temperature in degrees celsius
-     * @sample 25
+     *
+     * Example: 25
      */
     val temp: Int,
 )
